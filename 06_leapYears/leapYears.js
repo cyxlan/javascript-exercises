ac8cc0aaf03954bbdb5ac6788dc9b5f5a6ac120e
cyxlan <49080055+cyxlan@users.noreply.github.com>
2023-06-14 22:24:59 -0400

    Complete leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  function divisibleBy(num) {
+    if ((year / num) % 1 === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  if (divisibleBy(4) && (!divisibleBy(100) || divisibleBy(400))) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 // Do not edit below this line
