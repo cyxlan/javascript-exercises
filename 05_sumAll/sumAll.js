d92cdcfd744fc5ad3bd1fcaa0e20725811c997e9
cyxlan <49080055+cyxlan@users.noreply.github.com>
2023-06-14 22:11:37 -0400

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
 
+  let start;
+  let stop;
+  if (num1 <= num2) {
+    start = num1;
+    stop = num2;
+  }
+  else {
+    start = num2;
+    stop = num1;
+  }
+
+  sum = 0;
+  for (let i = start; i <= stop; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
