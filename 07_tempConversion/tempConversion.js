8f024b56c8463e9165a2578ac9dec34dc977f98c
cyxlan <49080055+cyxlan@users.noreply.github.com>
2023-06-14 22:45:07 -0400

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let result = (f - 32) * (5/9);
+  if (result % 1 !== 0) {
+    result = +result.toFixed(1);
+  }
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let result = c * (9/5) + 32;
+  if (result % 1 !== 0) {
+    result = +result.toFixed(1);
+  }
+  return result;
 };
 
 // Do not edit below this line
