36e4c03da4ffde174821368cc271e504d1b1c997
cyxlan <49080055+cyxlan@users.noreply.github.com>
2023-06-14 21:50:44 -0400

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+  for (let i = 0; i < values.length; i++) {
+    let value = values[i];
+    array = removeValue(value);
+  }
 
+  function removeValue(value) {
+    valueIndex = array.indexOf(value);
+    // if the value is not present, return the array unchanged
+    if (valueIndex === -1) {
+      return array;
+    }
+    arrayLeft = array.slice(0, valueIndex);
+    arrayRight = array.slice(valueIndex);
+    arrayRight.shift();
+    result = arrayLeft.concat(arrayRight);
+    return result;
+  }
+
+  return array;
 };
 
 // Do not edit below this line
