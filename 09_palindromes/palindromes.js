47899e31591f415a3582e28e52d80382fe010055
cyxlan <49080055+cyxlan@users.noreply.github.com>
2023-12-14 16:03:21 -0500

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // strip non-alphanumeric chars
+  let arr = str.toLowerCase().replaceAll(/\W/g, '').split('');
+  return arr.every((item, i) => item === arr.reverse()[i]);
 };
 
 // Do not edit below this line
