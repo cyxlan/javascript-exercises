5f12164171d9165ab0534fb289030338e5cbeb35
cyxlan <49080055+cyxlan@users.noreply.github.com>
2023-12-14 19:41:41 -0500

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestPerson;
+  people.reduce((oldestAge, currentPerson) => {
+    let currentAge;
+    if ("yearOfDeath" in currentPerson) {
+      currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    } else {
+      currentAge = new Date().getFullYear() - currentPerson.yearOfBirth;
+    }
+    if (oldestAge < currentAge) {
+      oldestPerson = currentPerson;
+      oldestAge = currentAge;
+    }
+    return oldestAge;
+  }, 0);
+  return oldestPerson;
 };
 
 // Do not edit below this line
